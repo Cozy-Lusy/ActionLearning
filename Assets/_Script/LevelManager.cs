ae4e472111d37125cb1696ba350d28f593dd2804
Cozy_Lusy <>
2022-04-23 19:04:02 +0300

    Continue

---
+++ Assets/_Script/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EventMoveQuestion();
+    }
+
+    void EventMoveQuestion()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if (moveX != 0 || moveY != 0)
+        {
+            
+        }
+    }
+}
---
+++ Assets/_Script/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+
+    private Vector2 moveDerection;
+
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDerection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDerection.x * moveSpeed, moveDerection.y * moveSpeed);
+    }
+}
