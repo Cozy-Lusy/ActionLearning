fb26adf26b2be95fe024956a0d11adbab24b57c4
Cozy_Lusy <>
2022-05-23 21:44:56 +0300

    fix main menu buttons

--- Assets/_Scripts/MainMenu.cs
+++ Assets/_Scripts/MainMenu.cs
@@ -2,10 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    [SerializeField] private Button buttonLoad;
+    [SerializeField] private Button buttonNewGame;
+    [SerializeField] private Button buttonQuit;
+
+    private const string saveKey = "mainKey";
+
+
+    private void OnEnable()
+    {
+        buttonLoad.onClick.AddListener(LoadGame);
+        buttonNewGame.onClick.AddListener(NewGame);
+        buttonQuit.onClick.AddListener(QuitGame);
+    }
+
+    private void OnDisable()
+    {
+        buttonLoad.onClick.RemoveAllListeners();
+        buttonNewGame.onClick.RemoveAllListeners();
+        buttonQuit.onClick.RemoveAllListeners();
+    }
+
+    public void LoadGame()
+    {
+        var data = SaveManager.Load<SaveData>(saveKey);
+
+        SceneManager.LoadScene(data.SceneIndex);
+    }
+
+    public void NewGame()
     {
         SceneManager.LoadScene(Constants.INCREMENT_INDEX_LEVEL);
     }
