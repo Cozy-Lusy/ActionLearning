600133986f4efc241e4c552aca6186f577430740
Cozy_Lusy <>
2022-05-24 23:24:17 +0300

    Fix questions dictionary

--- Assets/_Scripts/SettingsStorage.cs
+++ Assets/_Scripts/SettingsStorage.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class SettingsStorage : ScriptableObject
 {
     [SerializeField] private List<QuestionData> questionsForLevel1;
+    [SerializeField] private List<QuestionData> questionsForLevel2;
 
     private readonly Dictionary<int, List<QuestionData>> _storage = new Dictionary<int, List<QuestionData>>();
 
     public void Init()
     {
         _storage.Add(1, questionsForLevel1);
+        _storage.Add(2, questionsForLevel2);
     }
 
     public List<QuestionData> GetQuestions(int levelIndex)
@@ -24,5 +26,10 @@ public class SettingsStorage : ScriptableObject
         Debug.LogError("Вопросы не найдены");
 
         return new List<QuestionData>();
+    }
+
+    public void ClearStorage()
+    {
+        _storage.Clear();
     }
 }
